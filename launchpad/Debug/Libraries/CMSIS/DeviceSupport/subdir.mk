7ab4273e4759017151f972a5b8803435bdba8363
unknown <high1uck@pusan.ac.kr>
2018-12-05 20:20:04 +0900

    led onoff with lib

---
+++ launchpad/Debug/Libraries/CMSIS/DeviceSupport/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Libraries/CMSIS/DeviceSupport/system_stm32f10x.c 
+
+OBJS += \
+./Libraries/CMSIS/DeviceSupport/system_stm32f10x.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Libraries/CMSIS/DeviceSupport/%.o: ../Libraries/CMSIS/DeviceSupport/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM C Compiler 5'
+	armcc -I"C:\STM32_Launchpad\STM32_Launchpad\launchpad\Libraries\CMSIS\CoreSupport" -I"C:\STM32_Launchpad\STM32_Launchpad\launchpad\Libraries\CMSIS\DeviceSupport" -I"C:\STM32_Launchpad\STM32_Launchpad\launchpad\Libraries\CMSIS\DeviceSupport\Startup" -I"C:\STM32_Launchpad\STM32_Launchpad\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\inc" -I"C:\STM32_Launchpad\STM32_Launchpad\launchpad\Libraries\STM32F10x_StdPeriph_Driver_v3.5\src" -O2 --cpu=cortex-m3 -g -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
